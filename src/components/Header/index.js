2850470307e344de6ff35a6eb5983643a1996862
shubhamdangi <shubham.d3v@gmail.com>
2022-02-01 16:20:14 +0530

    Authentication Integrated overall

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,81 @@
-import React from "react";
-import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import {
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  NavDropdown,
+  NavLink,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, logout } from "../../firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
 
 function Header() {
-  return <header>heaad</header>;
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const fetchUserName = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+      setName(data.name);
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/");
+    fetchUserName();
+  }, [user, loading]);
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <NavLink>
+            <Navbar.Brand as={Link} to="/">
+              BraveHearts
+            </Navbar.Brand>
+          </NavLink>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              {user ? (
+                <NavDropdown title={name ? name : user?.email} id="username">
+                  <Link to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </Link>
+
+                  <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <>
+                  <NavLink>
+                    <Nav.Link as={Link} to="/login">
+                      Login
+                    </Nav.Link>
+                  </NavLink>
+                  <NavLink>
+                    <Nav.Link as={Link} to="/register">
+                      Register
+                    </Nav.Link>
+                  </NavLink>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
 export default Header;
